41713218b4d7fed7d15e34480aa2e9d31c86676d
Willian Rocha <hello@willgcr.me>
2026-05-28 01:57:26 -0300

    Switch Homebrew formula to source build, bump to 0.1.6

--- Formula/kc.rb
+++ Formula/kc.rb
@@ -1,18 +1,20 @@
 class Kc < Formula
   desc "Terminal calendar with CalDAV and Google Calendar sync"
   homepage "https://wrklabs.org/kc"
+  url "https://github.com/wrk-labs/kc/archive/refs/tags/v__VERSION__.tar.gz"
+  sha256 "__SHA__"
   version "__VERSION__"
   license "GPL-2.0-only"
+  head "https://github.com/wrk-labs/kc.git", branch: "master"
 
   depends_on arch: :arm64
   depends_on :macos
+  depends_on "pkg-config" => :build
   depends_on "libical"
   depends_on "ncurses"
 
-  url "https://brew.wrklabs.org/dist/kc/kc-__VERSION__-darwin-arm64.tar.gz"
-  sha256 "__SHA_ARM64__"
-
   def install
+    system "make"
     bin.install "kc"
   end
 
